b231ff14a0241ba61e04e026923f9c74fb065ee4
Chat43LFGV <81338012+Chat43LFGV@users.noreply.github.com>
2021-03-24 23:01:51 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyCuS1ZFd3OwnsHdUks5CACcuBUISTACAnM",
+    authDomain: "chatador-21e50.firebaseapp.com",
+    projectId: "chatador-21e50",
+    storageBucket: "chatador-21e50.appspot.com",
+    messagingSenderId: "180347942951",
+    appId: "1:180347942951:web:465364a7f88b54594fb602",
+    measurementId: "G-YEZ0XWGMN7"
+    });
